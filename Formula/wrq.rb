33ca86b44b4322be9ec44314155fc216f3bb56e9
msmhmorsi <msmhmorsi@connect.ust.hk>
2026-08-28 21:56:48 +0800

    build: publish Homebrew v0.1.1

--- Formula/wrq.rb
+++ Formula/wrq.rb
@@ -1,8 +1,8 @@
 class Wrq < Formula
   desc "Local-first command-line library for research papers"
   homepage "https://github.com/mohamedsobhi777/wrq"
-  url "https://github.com/mohamedsobhi777/wrq/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0e3d806a9d4fd5ac39931f218b2de43a438ad6bde617620c56d0de22904100ae"
+  url "https://github.com/mohamedsobhi777/wrq/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "505cda44aa68d8277b87a7fdce40631b083c5f91610fd2fa63ff563129308a82"
   license "MIT"
   head "https://github.com/mohamedsobhi777/wrq.git", branch: "main"
 
